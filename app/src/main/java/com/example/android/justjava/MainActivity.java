feff89c95fbb57caf253b7ce2f6119cd616f1d5a
davidfabian <fabiand.01@gmail.com>
2017-07-11 13:22:20 +0300

    order button now opens email app and populates a new email with order details

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -18,7 +21,10 @@ public class MainActivity extends AppCompatActivity {
     int price = 5;
     int whippedCreamPrice = 1;
     int chocoPrice = 2;
+    String name_entered;
+    String orderAddress = "order@justjava.com";
     private int minOrder = 1;
+    private int maxOrder = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,28 +33,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method is called when the order button is clicked. Displays a custom message.
+     * This method is called when the order button is clicked. Tries to create a custom email message to orderAddress with a custom message based on the order details.
      */
     public void submitOrder(View view) {
+//  /**
+//  * generates the order summary on the screen
+//  */
+//
+//        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+//        orderSummaryTextView.setText(createOrderSummary(priceDisplay()));
+        Intent sendOrderMail = new Intent(Intent.ACTION_SENDTO);
+        sendOrderMail.setData(Uri.parse("mailto:" + orderAddress));
+        sendOrderMail.putExtra(Intent.EXTRA_SUBJECT, "Order for: " + getName_entered());
+        sendOrderMail.putExtra(Intent.EXTRA_TEXT, createOrderSummary(priceDisplay()));
 
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(createOrderSummary(priceDisplay()));
+        if (sendOrderMail.resolveActivity(getPackageManager()) != null) {
+            startActivity(sendOrderMail);
+        }
     }
 
     /**
-     * It creates and prints the summary string for the order
+     * It creates the summary string for the order
      *
      * @param totalcost is the cost of the purchase
      * @return returns the complete, formatted string with the custom message
      */
     private String createOrderSummary(int totalcost) {
-        return ("Name: Director David" +
+        return ("Name: " + getName_entered() +
                 "\n" + getResources().getString(R.string.quantity) + ": " + current_number +
                 "\n" + getResources().getString(R.string.whipped_cream) + ": " + whippedCream() +
                 "\n" + getResources().getString(R.string.chocolate) + ": " + chocolateAdd() +
                 "\n" + getResources().getString(R.string.total) + ": " + NumberFormat.getCurrencyInstance().format(totalcost) +
                 "\n" + getResources().getString(R.string.thankyou));
 
+    }
+
+    /**
+     * extracts the name from the editText view
+     *
+     * @return the name entered
+     */
+    private String getName_entered() {
+        TextView nameField = (TextView) findViewById(R.id.name_input);
+        name_entered = nameField.getText().toString();
+        return name_entered;
     }
 
     /**
@@ -84,7 +112,12 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void increaseQty(View view) {
-        current_number += 1;
+        if (current_number >= maxOrder) {
+            current_number = maxOrder;
+            Toast.makeText(this, "You can't order more than " + maxOrder + " coffees at a time.", Toast.LENGTH_SHORT).show();
+        } else {
+            current_number += 1;
+        }
         setNumber(current_number);
     }
 
@@ -96,6 +129,7 @@ public class MainActivity extends AppCompatActivity {
     public void decreaseQty(View view) {
         if (current_number <= minOrder) {
             current_number = minOrder;
+            Toast.makeText(this, "You can't order less than " + minOrder + " coffees at a time.", Toast.LENGTH_SHORT).show();
         } else {
             current_number -= 1;
         }
